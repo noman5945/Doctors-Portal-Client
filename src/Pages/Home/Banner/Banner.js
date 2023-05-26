8586a98a9d945d4813d0bf85eb3a63d1c0ab6e79
noman27 <blt.normanbruce@yahoo.com>
2023-05-26 15:27:53 +0600

    Just die nobody needs you, you are outcast

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -3,8 +3,8 @@ import chair from "../../../assets/images/chair.png";
 
 const Banner = () => {
   return (
-    <div className="hero w-full py-10">
-      <div className="bg-banner-chair bg-cover bg-no-repeat hero-content  p-12 flex-col lg:flex-row-reverse">
+    <div className="bg-banner-chair bg-cover bg-no-repeat hero w-full py-10">
+      <div className="hero-content  p-12 flex-col lg:flex-row-reverse">
         <img src={chair} className="w-1/2 rounded-lg shadow-2xl" alt="" />
         <div className="w-1/2">
           <h1 className="text-5xl font-bold">Your Smile Starts Here</h1>
--- src/Pages/Home/UnderBanner/UnderBanner.js
+++ src/Pages/Home/UnderBanner/UnderBanner.js
@@ -4,20 +4,33 @@ import location from "../../../assets/icons/marker.svg";
 import phone from "../../../assets/icons/phone.svg";
 
 const UnderBanner = () => {
+  const openingTime = "10 AM - 10 PM";
+  const loc = "Brooklyn,NY 10036,United States";
+  const phoneNo = "01552321409";
+
   return (
     <div className="mx-auto">
       <div className="mt-0 grid grid-cols-1 lg:grid-cols-3 gap-3 ">
         <div className="bg-secondary border rounded-md py-2 flex flex-row">
           <img src={clock} alt="clock" />
-          <div>Opening Hours</div>
+          <div className="block text-white font-bold m-6">
+            <h2>Opening Hours</h2>
+            <p>{openingTime}</p>
+          </div>
         </div>
         <div className="bg-neutral border rounded-md py-2 flex flex-row">
           <img src={location} alt="location-mark" />
-          <div>Visit Our Location</div>
+          <div className="block text-white font-bold m-6">
+            <h2>Visit Our Location</h2>
+            <p>{loc}</p>
+          </div>
         </div>
         <div className="bg-secondary border rounded-md py-2 flex flex-row">
           <img src={phone} alt="phone" />
-          <div>Contact Us Now</div>
+          <div className="block text-white font-bold m-6">
+            <h2>Contact Us</h2>
+            <p>{phoneNo}</p>
+          </div>
         </div>
       </div>
     </div>
